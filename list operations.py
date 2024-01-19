eed30304a01c3dcfeec9127e51bae62feb632e61
Sarbik Sinha Ray <sarbiksinharay@users.noreply.github.com>
2024-01-19 23:16:42 +0530

    Create list operations.py

---
+++ list operations.py
@@ -0,0 +1,14 @@
+list=[1,2,3,4]
+list.reverse()
+print(list)
+list.sort()
+print(list)
+list.insert(1,100)
+print(list)
+list.remove(3)
+print(list)
+list.pop(3)
+print(list)
+lst=["banana","chery","apple"]
+list.extend(lst)
+print(list)
