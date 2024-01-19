5e88dd6a304338c7eb05d4037f61251da529b4e8
Sarbik Sinha Ray <sarbiksinharay@users.noreply.github.com>
2024-01-19 23:16:38 +0530

    Create python list sort,insert.py

---
+++ python list sort,insert.py
@@ -0,0 +1,13 @@
+list=[1,2,3,4,5]
+list.reverse()
+print(list)
+list2=[0,4,12,78,13,48,13,75,459,25,5626,5]
+list2.sort()
+print(list2)
+list2.insert(0,"NEW LIST")
+print(list2)
+list2.insert(3,"HI")
+print(list2)
+
+
+
