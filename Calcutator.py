4a8c0c52361fe8fbad9d26b87395c016b879efc2
Sarbik Sinha Ray <sarbiksinharay@users.noreply.github.com>
2024-01-19 23:16:49 +0530

    Create Calcutator.py

---
+++ Calcutator.py
@@ -0,0 +1,12 @@
+num1=int(input("ENTER THE FIRST NUMBER:\n"))
+num2=int(input("ENTER THE SECOND NUMBER:\n"))
+sum=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+mod=num1%num2
+print("SUM OF",num1 ,"and",num2,"is= ",sum)
+print("SUBTRACTION OF",num1 ,"and",num2,"is= ",sub)
+print("MULTIPLICATION OF",num1 ,"and",num2,"is= ",mul)
+print("DIVISION OF",num1 ,"and",num2,"is= ",div)
+print("MODULUS OF",num1 ,"and",num2,"is= ",mod)
