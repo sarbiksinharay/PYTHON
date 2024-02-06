90416690d41ef225a694a9d31ebd7cfd71399792
Sarbik Sinha Ray <sarbiksinharay@users.noreply.github.com>
2024-02-06 10:38:35 +0530

    Create even & odd.py

---
+++ even & odd.py
@@ -0,0 +1,7 @@
+number=int(input("ENTER THE NUMBER:"))
+if number%2!=0:
+    print(number,"is a odd number")
+elif number==0:
+    print("0 is an neutral number")
+else:
+    print(number,"is a even number")
