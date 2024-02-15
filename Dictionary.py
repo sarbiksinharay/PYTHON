5db9f8379209f78cd5c3b0e38d65c08e366b95e6
Sarbik Sinha Ray <sarbiksinharay@users.noreply.github.com>
2024-02-15 20:04:27 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,14 @@
+info_sarbik={
+    "name" : "SARBIK SINHA RAY",
+    "age" : 19,
+    "sgpa" : 7.70,
+    "mob_no" : 6290922505,
+    "subjects": ("MATHEMATICS","PYTHON & LOGIC DESIGN","MDC","DATA ANALYTICS","EXPERIENTIAL LEARNING","FULL STACK WEB DEVELPOMENT"),
+    "grades" : {
+        "math" : "A+",
+        "phy" : "B+",
+        "programming" : "A+"
+
+    }
+}
+print(info_sarbik["grades"]["phy"])
