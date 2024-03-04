4f64a756894b36e3ff111cf8c60b098a6eb9340b
Sarbik Sinha Ray <sarbiksinharay@users.noreply.github.com>
2024-03-04 20:11:36 +0530

    Create Even and odd numbers within a range using continue.py

---
+++ Even and odd numbers within a range using continue.py
@@ -0,0 +1,15 @@
+i=1
+while i<=10:
+   if(i%2==0):
+       i+=1
+       continue
+   print(i,"\n")
+   i += 1
+
+i=1
+while i<=10:
+    if(i%2==1):
+        i+=1
+        continue
+    print(i)
+    i+=1
